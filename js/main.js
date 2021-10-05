5aaf9a340be185a966d8d5cb14274f5e399c0498
anna <kiri35627@gmail.com>
2021-10-06 00:42:15 +0300

    utils deleted, main.js updated

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 // https://stackoverflow.com/questions/45735472/generate-a-random-number-between-2-values-to-2-decimals-places-in-javascript/45736188
 
-function genRand(min, max, decimalPlaces) {
-  if (min < 0){
+function getRandomFloatNumber(min, max, decimalPlaces) {
+  if (min < 0 || max < 0){
     return 'Error';
   }
   return (Math.random() * (max - min) + min).toFixed(decimalPlaces) * 1;
 }
-genRand(0, 100, 8);
+getRandomFloatNumber(0, 100, 8);
+
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_random_function2
+
+function getRandomWholeNumber(min, max) {
+  if (min < 0 || max < 0){
+    return 'Error';
+  }
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+getRandomWholeNumber(0, 100);
